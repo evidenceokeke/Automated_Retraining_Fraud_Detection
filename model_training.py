0ed0ec9377d52952357ee81a2288fd264d5d6da9
evidenceokeke <evidenceisokeke@gmail.com>
2025-04-03 14:21:23 -0400

    first commit

--- model_training.py
+++ model_training.py
@@ -7,11 +7,18 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.pipeline import Pipeline
 import joblib
+import boto3
 
 
 # Set the random seed for reproducibility
 random.seed(42)
 np.random.seed(42)
+
+# Initialize boto3 client for AWS S3
+s3 = boto3.client('s3')
+
+# Download the dataset from S3
+s3.download_file('cc-fraud-detection-model-dataset','s3://cc-fraud-detection-model-dataset/dataset/creditcard.csv', 'dataset/creditcard.csv')
 
 # Load the dataset
 cc_fraud_df = pd.read_csv("dataset/creditcard.csv")
@@ -24,12 +31,15 @@ y = cc_fraud_df['Class']
 # Split the dataset: One for training, One  to simulate retraining
 X_train, X_new, y_train, y_new = train_test_split(X, y, test_size=0.2, stratify=y, random_state=42)
 
-# Save the splits to CSV files so that we can access them in retraining.py
-X_train.to_csv('dataset/X_train.csv', index=False)
+# To avoid continually save the dataset everytime it is run, run this only once
+"""# Save the new data (X_new, y_new) to S3 for future retraining simulation
 X_new.to_csv('dataset/X_new.csv', index=False)
-y_train.to_csv('dataset/y_train.csv', index=False)
 y_new.to_csv('dataset/y_new.csv', index=False)
 
+# Upload X_new and y_new to S3
+s3.upload_file('dataset/X_new.csv', 'cc-fraud-detection-model-dataset', 's3://cc-fraud-detection-model-dataset/dataset/X_new.csv')
+s3.upload_file('dataset/y_new.csv', 'cc-fraud-detection-model-dataset', 's3://cc-fraud-detection-model-dataset/dataset/y_new.csv')
+"""
 # Build and train the model, added scaling to the pipeline for preprocessing features
 model = Pipeline([
     ('scaler', StandardScaler()),
@@ -40,3 +50,6 @@ model.fit(X_train, y_train)
 
 # Move to pkl file
 joblib.dump(model, "fraud_pipeline.pkl")
+
+# Upload the trained model to S3 after training
+s3.upload_file('fraud_pipeline.pkl', 'cc-fraud-detection-model-dataset', 's3://cc-fraud-detection-model-dataset/fraud_pipeline.pkl')
