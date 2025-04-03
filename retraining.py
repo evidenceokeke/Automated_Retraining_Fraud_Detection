08b5abbfe51ab187a043334308aa8b8753afa076
evidenceokeke <evidenceisokeke@gmail.com>
2025-04-03 15:18:31 -0400

    first commit

--- retraining.py
+++ retraining.py
@@ -2,10 +2,6 @@
 import numpy as np
 import pandas as pd
 import random
-from sklearn.preprocessing import StandardScaler
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import train_test_split
-from sklearn.pipeline import Pipeline
 import joblib
 import boto3
 
